6fa7dd3c2a4135de3b5fa9a01514c772d5681b6b
ali <ali008009@yahoo.com>
2020-04-12 02:47:23 +0100

    Updates

---
+++ precache-manifest.8d21b0e92638851b1f57766abdc5413a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486e70c2701114c34499bd273e14e784",
+    "url": "/covid-19App/index.html"
+  },
+  {
+    "revision": "dcf7cc46a6eef4294f54",
+    "url": "/covid-19App/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "99302ceac2e1cbaf46d3",
+    "url": "/covid-19App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dcf7cc46a6eef4294f54",
+    "url": "/covid-19App/static/js/2.2b8467ae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19App/static/js/2.2b8467ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99302ceac2e1cbaf46d3",
+    "url": "/covid-19App/static/js/main.ac7ff196.chunk.js"
+  },
+  {
+    "revision": "08a2648d9534d38752e0",
+    "url": "/covid-19App/static/js/runtime-main.ec942606.js"
+  }
+]);
